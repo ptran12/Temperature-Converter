a476aff971848d4c4b1f7010a5bf750be0a9a61b
Phi Tran <phitran@Phis-MBP.attlocal.net>
2019-03-06 17:13:24 -0600

    fahrenheit function working

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const celciusInput = document.querySelector("#celcius > input");
 const fahrenheitInput = document.querySelector("#fahrenheit > input");
 const kelvinInput = document.querySelector("#kelvin > input");
 
-celciusInput.addEventListener("input", function () {
+
+function celciusToFahrenheitAndKelvin(){
     const cTemp = parseFloat(celciusInput.value); //converting string to number
     // equation to convert celcius to fahrenheit
     const fTemp = (cTemp * (9 / 5)) + 32;
@@ -11,4 +12,22 @@ celciusInput.addEventListener("input", function () {
     
     fahrenheitInput.value = fTemp;
     kelvinInput.value = kTemp;
-});
+}
+
+function fahrenheitToCelciusAndKelvin (){
+    const fTemp = parseFloat(fahrenheitInput.value);
+    //equation to convert fahrenheit to celcius 
+    const cTemp = (fTemp - 32) * (5/9);
+    const kTemp = (fTemp + 459.67) * 5/9
+
+    // displaying on page 
+    celciusInput.value = cTemp;
+    kelvinInput.value = kTemp;
+
+
+
+}
+
+celciusInput.addEventListener("input", celciusToFahrenheitAndKelvin);
+
+fahrenheitInput.addEventListener("input", fahrenheitToCelciusAndKelvin);
